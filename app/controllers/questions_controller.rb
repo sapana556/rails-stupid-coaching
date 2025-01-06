5e8456687019a48df38fcd64e2839ad48fe937b0
Sapana Fisher <187521469+sapana556@users.noreply.github.com>
2025-01-06 16:18:09 +0000

    all done until styling

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == "I am going to work"
+     @answer = "Great!"
+    else
+     @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
